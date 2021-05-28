a65f124dd266837c13c0222bb5b5531c705459cc
Salomon Fabian <salofabian6@gmail.com>
2021-05-28 09:54:20 -0600

    primer commit

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,39 @@
+import { Component, enableProdMode } from '@angular/core';
+
+
+@Component({
+
+    selector: 'app-heroe',
+    templateUrl: './heroe.component.html'
+
+})
+
+export class HeroeComponent{
+
+    nombre: string = "Ironman";
+    edad  : number = 45;
+
+    get nombreCapitalizado(): string{
+        return this.nombre.toUpperCase();
+    }
+
+    obtenerNombre(): string{
+        return `${ this.nombre } - ${ this.edad }`;
+
+    }
+
+    cambiarNombre():void{
+
+        if(this.nombre == "Ironman"){
+            this.nombre = 'Spiderman';
+        }else{
+            this.nombre = 'Ironman';
+        }
+        
+    }
+
+    cambiarEdad():void{
+        this.edad = 25;
+    }
+
+}
